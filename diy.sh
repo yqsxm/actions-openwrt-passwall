6f583a7a14e61f0240d220934c866ed048d5b8d1
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-03-05 19:06:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/"DISTRIB_DESCRIPTION='OpenWrt '"/"DISTRIB_DESCRIPTION='Lienol '"/g' package/default-settings/files/zzz-default-settings
+sed -i 's/"DISTRIB_REVISION='R20.1.24'"/"DISTRIB_REVISION='19.07'"/g' package/default-settings/files/zzz-default-settings
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
