16aee5a71833217e997b2995fe1eba6ec04775a2
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-03-12 20:56:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,4 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/kenzok8/litte.git package/litte
-rm -rf package/litte/luci-app-passwall
+
