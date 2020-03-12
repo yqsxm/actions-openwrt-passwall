bbdaa3e3c4ffea1022b18388e80bb7a2eec178e5
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-03-12 22:20:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,10 +7,9 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/kenzok8/litte.git
+git clone https://github.com/kenzok8/litte.git package/litte
 rm -rf package/litte/luci-app-passwall
 rm -rf package/litte/luci-app-smartdns
 rm -rf feeds/lienol/lienol/luci-theme-bootstrap-mod
 sed -i 's/"DISTRIB_DESCRIPTION='OpenWrt '"/"DISTRIB_DESCRIPTION='Lienol '"/g' package/default-settings/files/zzz-default-settings
 sed -i 's/"DISTRIB_REVISION='19.07-SNAPSHOT'"/"DISTRIB_REVISION='litte'"/g' package/default-settings/files/zzz-default-settings
-
