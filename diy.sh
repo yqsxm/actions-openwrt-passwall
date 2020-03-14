977b1a47200b345acacab351158e920a044c2fd9
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-03-14 21:56:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,3 +11,7 @@ git clone https://github.com/kenzok8/litte.git package/litte
 rm -rf package/litte/luci-app-passwall
 rm -rf package/litte/luci-app-smartdns
 rm -rf feeds/lienol/lienol/luci-theme-bootstrap-mod
+sed -i 's/"DISTRIB_DESCRIPTION='OpenWrt '"/"DISTRIB_DESCRIPTION='Lienol '"/g' package/default-settings/files/zzz-default-settings
+sed -i 's/"DISTRIB_REVISION='19.07-SNAPSHOT'"/"DISTRIB_REVISION='litte'"/g' package/default-settings/files/zzz-default-settings
+
+
