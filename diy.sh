c63f4035befd9840aed8ef55eb45dfd285d32259
kenzok8 <kenzok8@gmail.com>
2020-05-26 15:43:36 +0800

    Signed-off-by: kenzo

--- diy.sh
+++ diy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #=================================================
 # Description: Build OpenWrt using GitHub Actions
-# Lisence: MIT
 # Author: kenzo
 # https://github.com/kenzok8
 #=================================================
@@ -9,8 +8,9 @@
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/litte.git package/litte
 date=`date +%m.%d.%Y`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D by kenzo'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
+cp -f default-settings package/*/*/default-settings/files/zzz-default-settings
 ./scripts/feeds update -a
-./scripts/feeds install
+./scripts/feeds install -a
 
