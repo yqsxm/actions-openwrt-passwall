06da83e194cc76618f8343e110f4412c4f288725
yqsxm <66452880+yqsxm@users.noreply.github.com>
2020-07-10 19:18:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,7 +5,7 @@
 # https://github.com/kenzok8
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.199/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/litte.git package/litte
 date=`date +%m.%d.%Y`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D by kenzo'/g" package/base-files/files/etc/openwrt_release
