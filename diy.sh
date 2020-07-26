c1dc478901746f32adf5759769a326b6137f6564
yqsxm <66452880+yqsxm@users.noreply.github.com>
2020-07-26 08:46:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -16,10 +16,19 @@ git clone https://github.com/tty228/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome
 git clone https://github.com/pymumu/luci-app-smartdns
 git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
+#OpenAppFilter（应用过滤）：
+#git clone https://github.com/destan19/OpenAppFilter
+#KoolproxyR（去广告）：
+git clone https://github.com/jefferymvp/luci-app-koolproxyR
+#ServerChan（微信推送）：
+#git clone https://github.com/tty228/luci-app-serverchan
+#Jd-Dailybonus（京东自动签到）
+cd lede/package/lean/ 
+git clone https://github.com/jerrykuku/node-request.git  #git node-request 依赖
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
 date=`date +%m.%d.%Y`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D by kenzo'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 cp -f default-settings package/*/*/default-settings/files/zzz-default-settings
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-
